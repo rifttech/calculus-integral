54553c59ed4dfc6f63045fddfdccc066490b9646
Arthur Abramov <rifttech.arthur@gmail.com>
2018-07-15 15:30:02 +0300

    add midpont rule

--- spec/integrals/Integrals_spec.cr
+++ spec/integrals/Integrals_spec.cr
@@ -3,11 +3,29 @@ require "../../src/integrals/Integrals.cr"
 
 describe Integrals do
     describe "#riemann" do
-        it "calcs riemann summ" do
-            result = Integrals.riemann(4.0, 10.0, 1000.0, 3) do |x|
+        it "calcs riemann left summ" do
+            result = Integrals.riemann_right(4.0, 10.0, 1000.0, 3) do |x|
                 x * x
             end
-            puts "result is #{result}"
+            puts "riemann right summ is #{result}"
+        end
+    end
+
+    describe "#riemann" do
+        it "calcs riemann left summ" do
+            result = Integrals.riemann_left(4.0, 10.0, 1000.0, 3) do |x|
+                x * x
+            end
+            puts "riemann left sum is #{result}"
+        end
+    end
+
+    describe "#riemann_midpoit" do
+        it "calcs midpoint rule" do
+            result = Integrals.riemann_midpoint(4.0, 10.0, 1000.0, 3) do |x|
+                x * x
+            end
+            puts "riemann_midpoit is #{result}"
         end
     end
 end
--- src/integrals/Integrals.cr
+++ src/integrals/Integrals.cr
@@ -1,5 +1,17 @@
 module Integrals extend self
-    def riemann(a : Float32, b : Float32, step : Float32, precision : Int32 = 2) : Float32
+    def riemann_left(a : Float32, b : Float32, step : Float32, precision : Int32 = 2) : Float32
+        dx = (b-a) / step
+        i = a
+        sum = 0.0_f32    
+        while i <= b 
+            f = yield i
+            i += dx
+            sum += dx * f
+        end
+        sum.round precision
+    end
+
+    def riemann_right(a : Float32, b : Float32, step : Float32, precision : Int32 = 2) : Float32
         dx = (b-a) / step
         i = a
         sum = 0.0_f32    
@@ -8,7 +20,19 @@ module Integrals extend self
             f = yield i
             sum += dx * f
         end
-
         sum.round precision
+    end
+
+
+    def riemann_midpoint(a : Float32, b : Float32, step : Float32, precision : Int32 = 2) : Float32
+        dx = (b-a) / step
+        sum = 0.0_f32
+        count = 1.0;    
+        while count < step 
+            f = yield a + dx * (count + 0.5)
+            count = count + 1.0
+            sum += f
+        end
+        (sum * dx).round precision
     end
 end
